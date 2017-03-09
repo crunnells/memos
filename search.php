355f7403892d4a17f4c8a727e14218bc749aee69
Chris Runnells <chris@midpacificdesign.com>
2017-03-08 16:10:51 -1000

    Finalized CSS for review
    Updated RTL and Editor styles, and formatted style.css according to WP
    CSS style

--- search.php
+++ search.php
@@ -18,6 +18,9 @@ get_header(); ?>
 			<header class="page-header">
 				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'memos' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header>
+
+			<?php get_search_form(); ?>
+
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
